ef0c02ee5b072f8ed1233663f473f2dc6a2d4a69
nishant4tech <nish4tech@gmail.com>
2024-06-05 06:43:38 -0400

    Initial commit of KMM project

---
+++ shared/src/androidMain/kotlin/com/ip/mobile/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.ip.mobile
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
