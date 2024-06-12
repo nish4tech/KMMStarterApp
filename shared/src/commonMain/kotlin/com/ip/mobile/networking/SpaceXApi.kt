cc1e631e08367954646e808d9fa6a1cb818403f0
nishant4tech <nish4tech@gmail.com>
2024-06-12 15:27:09 -0400

    data fetch changes

--- shared/src/commonMain/kotlin/com/ip/mobile/networking/SpaceXApi.kt
+++ shared/src/commonMain/kotlin/com/ip/mobile/networking/SpaceXApi.kt
@@ -3,12 +3,14 @@ package com.ip.mobile.networking
 import com.ip.mobile.home.data.RocketLaunch
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
+import io.ktor.client.request.headers
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.flow.Flow
 import kotlinx.serialization.json.Json
@@ -20,6 +22,8 @@ class SpaceXApi {
         }
         install(ContentNegotiation) {
             json(Json {
+                prettyPrint = true
+                isLenient = true
                 ignoreUnknownKeys = true
                 useAlternativeNames = false
             })
@@ -32,10 +36,16 @@ class SpaceXApi {
                 }
             }
         }
+        install(DefaultRequest) {
+            headers {
+                //append("apikey", "")
+            }
+        }
 
     }
 
     suspend fun getAllLaunches(): List<RocketLaunch> {
+        //todo check response type, if not 200 then show error
         return httpClient.get("https://api.spacexdata.com/v5/launches").body()
     }
 
