ae90694b38bad27e983929f66bdd01f1abd62243
nishant4tech <nish4tech@gmail.com>
2024-06-13 13:09:45 -0400

    data fetch changes

--- shared/src/commonMain/kotlin/com/ip/mobile/networking/SpaceXApi.kt
+++ shared/src/commonMain/kotlin/com/ip/mobile/networking/SpaceXApi.kt
@@ -6,48 +6,68 @@ import io.ktor.client.call.body
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
 import io.ktor.client.request.headers
+import io.ktor.http.URLProtocol
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.flow.Flow
 import kotlinx.serialization.json.Json
 
 class SpaceXApi {
-    private val httpClient = HttpClient {
-        install(HttpTimeout) {
-            requestTimeoutMillis = 15_000
-        }
-        install(ContentNegotiation) {
-            json(Json {
-                prettyPrint = true
-                isLenient = true
-                ignoreUnknownKeys = true
-                useAlternativeNames = false
-            })
-        }
-        install(Logging) {
-            level = LogLevel.ALL
-            logger = object : Logger {
-                override fun log(message: String) {
-                    println(message)
+
+    companion object {
+        const val BASE_URL = "api.spacexdata.com"
+    }
+
+    fun prepareClient(partnerId: String, authToken: String): HttpClient = HttpClient {
+            expectSuccess = true
+            install(HttpTimeout) {
+                requestTimeoutMillis = 5 * 60 * 1000
+                connectTimeoutMillis = 5 * 60 * 1000
+            }
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+            install(Logging) {
+                level = LogLevel.ALL
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        println(message)
+                    }
+                }
+            }
+            install(DefaultRequest) {
+                headers {
+                    append("Authorization", "Bearer $authToken")
+                    append("PartnerID", partnerId)
+                }
+            }
+
+            defaultRequest {
+                host = BASE_URL
+                url {
+                    protocol = URLProtocol.HTTPS
                 }
             }
         }
-        install(DefaultRequest) {
-            headers {
-                //append("apikey", "")
-            }
-        }
-
-    }
 
     suspend fun getAllLaunches(): List<RocketLaunch> {
         //todo check response type, if not 200 then show error
-        return httpClient.get("https://api.spacexdata.com/v5/launches").body()
+        val httpClient = prepareClient("", "")
+        return httpClient.get("v5/launches").body()
     }
 
-
 }
+
+
+
+
+
