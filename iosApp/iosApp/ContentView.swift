77146425df5257c754584a6236fa824908ff2883
nishant4tech <nish4tech@gmail.com>
2024-06-18 23:25:03 -0400

    iOS version

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,57 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
+    @ObservedObject private(set) var viewModel: ViewModel
 
 	var body: some View {
-		Text(greet)
-	}
+        NavigationView {
+            listView()
+            .navigationBarTitle("SpaceX Launches")
+            .navigationBarItems(trailing:
+                Button("Reload") {
+                    self.viewModel.loadLaunches()
+            })
+        }
+    }
+    
+    private func listView() -> AnyView {
+            switch viewModel.launches {
+            case .loading:
+                return AnyView(Text("Loading...").multilineTextAlignment(.center))
+            case .result(let launches):
+                return AnyView(Text("Data came -> \(launches.count)").multilineTextAlignment(.center))
+            case .error(let description):
+                return AnyView(Text(description).multilineTextAlignment(.center))
+            }
+        }
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+extension ContentView {
+    enum LoadableLaunches {
+        case loading
+        case result([RocketLaunch])
+        case error(String)
+    }
+    
+    @MainActor
+    class ViewModel: ObservableObject {
+        @Published var launches = LoadableLaunches.loading
+        
+        
+        init() {
+            self.loadLaunches()
+        }
+        
+        func loadLaunches() {
+            Task {
+                do {
+                    self.launches = .loading
+                    let launches = try await SpaceXRepo().getAllLaunches()
+                    self.launches = .result(launches)
+                } catch {
+                    self.launches = .error(error.localizedDescription)
+                }
+            }
+        }
+    }
 }
